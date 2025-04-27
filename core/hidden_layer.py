3dffb8168d071d853013f6bb24d7104206a550ba
RYueV <yue.racheva@gmail.com>
2025-04-28 02:19:40 +0300

    Переход на классическое STDP

--- core/hidden_layer.py
+++ core/hidden_layer.py
@@ -1,6 +1,6 @@
 import numpy as np
-import core.hidden_config as h_conf
-from core.learning import update_weights_stdp, apply_inhibition
+import core.global_config as cfg
+from core.learning import update_weights_stdp
 
 
 """
@@ -14,32 +14,32 @@ from core.learning import update_weights_stdp, apply_inhibition
 # Инициализация скрытого слоя
 def init_hidden_layer():
     # Количество входов (на каждый пиксель 2 состояния)
-    input_size = h_conf.IMAGE_HEIGHT * h_conf.IMAGE_WIDTH * 2
+    input_size = cfg.IMAGE_HEIGHT * cfg.IMAGE_WIDTH * 2
     # Инициализация весов
     weights = np.clip(
         np.random.normal(
-            h_conf.W_INIT_MEAN, h_conf.W_INIT_STD, (h_conf.COUNT_NEURONS, input_size)
+            cfg.W_INIT_MEAN, cfg.W_INIT_STD, (cfg.COUNT_NEURONS, input_size)
         ),
-        h_conf.W_MIN, h_conf.W_MAX
+        cfg.W_MIN, cfg.W_MAX
     ).astype(np.float32)
 
     return {
         # Текущее значение потенциала для каждого нейрона сети
-        "membrane_potentials": np.zeros(h_conf.COUNT_NEURONS, np.float32),
+        "membrane_potentials": np.zeros(cfg.COUNT_NEURONS, np.float32),
         # Время последней активации каждого входа
         "last_input_times": np.zeros(input_size, np.float32),
         # Время последнего обновления потенциала каждого нейрона сети
-        "last_update": np.zeros(h_conf.COUNT_NEURONS, np.float32),
+        "last_update": np.zeros(cfg.COUNT_NEURONS, np.float32),
         # Время последнего спайка каждого нейрона сети
-        "last_spike": np.full(h_conf.COUNT_NEURONS, -np.inf, np.float32),
+        "last_spike": np.full(cfg.COUNT_NEURONS, -np.inf, np.float32),
         # Время окончания периода ингибирования для каждого нейрона
-        "inhibited_until": np.zeros(h_conf.COUNT_NEURONS, np.float32),
+        "inhibited_until": np.zeros(cfg.COUNT_NEURONS, np.float32),
         # Матрица весов
         "weights": weights,
         # Текущее время симуляции
         "current_ms": None,
         # Накопленный вклад событий за текущую миллисекунду.
-        "batch_sum": np.zeros(h_conf.COUNT_NEURONS, np.float32),
+        "batch_sum": np.zeros(cfg.COUNT_NEURONS, np.float32),
         # Массив спайков: (момент времени, номер нейрона)
         "spikes": []
     }
@@ -71,18 +71,18 @@ def _event_processing(
     u = state["membrane_potentials"]
     # Экспоненциальное затухание потенциалов
     dt = t_ms - state["last_update"]
-    u *= np.exp(-dt / h_conf.TAU_LEAK)
+    u *= np.exp(-dt / cfg.TAU_LEAK)
     state["last_update"][:] = t_ms
 
     # Добавляем вклад только тем нейронам, которые не подавлены
     mask_active = (
         (t_ms >= state["inhibited_until"]) &
-        (t_ms >= state["last_spike"] + h_conf.T_REF)
+        (t_ms >= state["last_spike"] + cfg.T_REF)
     )
     u[mask_active] += state["batch_sum"][mask_active]
 
     # Если были спайки у одного или нескольких нейронов
-    gave_spike = np.where(u > h_conf.I_THRES)[0]
+    gave_spike = np.where(u > cfg.I_THRES)[0]
     if gave_spike.size > 0:
         # Победителем считаем нейрон с максимальным потенциалом
         winner_index = gave_spike[np.argmax(u[gave_spike])]
@@ -91,28 +91,16 @@ def _event_processing(
         state["last_spike"][winner_index] = t_ms
         u[winner_index] = 0.0
         # Латеральное торможение
-        apply_inhibition(
-            inhibited_until_array=state["inhibited_until"],
-            t=t_ms,
-            spiking_index=winner_index
-        )
+        mask_inhibit = np.arange(cfg.COUNT_NEURONS) != winner_index
+        state["inhibited_until"][mask_inhibit] = t_ms + cfg.T_INHIBIT
+
         # Если сеть обучается, то обновляем веса для победителя по правилу STDP
         if train:
             update_weights_stdp(
-                neuron_index=winner_index,
                 t_post=t_ms,
-                weights=state["weights"],
+                synapse_weights=state["weights"][winner_index],
                 last_input_times=state["last_input_times"]
             )
-            all_neurons = np.arange(h_conf.COUNT_NEURONS)
-            losers = all_neurons[all_neurons != winner_index]
-            for loser in losers:
-                w = state["weights"][loser]
-                dw_ltd = (0.1 * h_conf.ALPHA_MINUS) * np.exp(
-                    -h_conf.BETA_MINUS * (h_conf.W_MAX - w) / h_conf.W_RANGE
-                )
-                w -= dw_ltd
-                np.clip(w, h_conf.W_MIN, h_conf.W_MAX, out=w)
 
     # Очищаем накопленное
     state["batch_sum"].fill(0.0)
@@ -130,7 +118,7 @@ def hidden_layer_step(
     # События группируются по миллисекундам
     t_ms = int(t)
     # Определяем индекс входа
-    input_id = 2 * (y * h_conf.IMAGE_WIDTH + x) + p
+    input_id = 2 * (y * cfg.IMAGE_WIDTH + x) + p
 
     # При смене миллисекунды обрабатываем группу предыдущих событий
     if state["current_ms"] is None:
--- core/learning.py
+++ core/learning.py
@@ -1,40 +1,29 @@
 import numpy as np
-
-# Глобальные константы для скрытого слоя
-import core.hidden_config as h_conf
+import core.global_config as cfg
 
 
 
-# Обновление весов нейрона по правилу STDP
-def update_weights_stdp(
-        neuron_index,       # индекс нейрона       
+# Обновление весов победившего нейрона по правилу STDP
+def update_weights_stdp(     
         t_post,             # время спайка нейрона
-        weights,            # вектор весов нейрона
+        synapse_weights,    # вектор весов нейрона
         last_input_times    # вектор, который хранит время активации каждого входа
 ):
-    # Строка весов связей нейрона со всеми возможными входами
-    synapse_weights = weights[neuron_index]
     # Разница между моментами времени, когда был спайк и когда пришел входной сигнал
     delta_t = t_post - last_input_times
     # Окно времени, в пределах которого можно считать, что входной сигнал пришел незадолго до спайка
-    ltp_mask = np.logical_and(delta_t > 0.0, delta_t < h_conf.T_LTP)
+    ltp_mask = (delta_t > 0.0) & (delta_t < cfg.T_LTP)
 
     # Вектор коэффициентов усиления связей
-    dw_ltp = h_conf.ALPHA_PLUS * np.exp(-h_conf.BETA_PLUS * (synapse_weights - h_conf.W_MIN) / h_conf.W_RANGE)
+    dw_ltp = cfg.ALPHA_PLUS * np.exp(-cfg.BETA_PLUS * (synapse_weights - cfg.W_MIN) / cfg.W_RANGE)
     # Вектор коэффициентов ослабления связей
-    dw_ltd = h_conf.ALPHA_MINUS * np.exp(-h_conf.BETA_MINUS * (h_conf.W_MAX - synapse_weights) / h_conf.W_RANGE)
+    dw_ltd = cfg.ALPHA_MINUS * np.exp(-cfg.BETA_MINUS * (cfg.W_MAX - synapse_weights) / cfg.W_RANGE)
 
     # Если входной сигнал пришел незадолго до того, как нейрон активировался, усиливаем связь
     synapse_weights +=  ltp_mask * dw_ltp 
     # Иначе ослабляем
     synapse_weights -= (~ltp_mask) * dw_ltd
     # Ограничиваем веса в допустимом диапазоне
-    np.clip(synapse_weights, h_conf.W_MIN, h_conf.W_MAX, out=synapse_weights)
+    np.clip(synapse_weights, cfg.W_MIN, cfg.W_MAX, out=synapse_weights)
 
-
-
-# Ингибирование всех нейронов, кроме сработавшего
-def apply_inhibition(inhibited_until_array, t, spiking_index):
-    inhibited_until_array[:] = t + h_conf.T_INHIBIT
-    inhibited_until_array[spiking_index] = 0.0
 
